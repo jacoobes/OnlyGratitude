c91d3a31f11ced61b0e0d992a027b15fd66d75cf
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2023-04-21 23:49:49 -0500

    first commit

---
+++ composeApp/build.gradle.kts
@@ -0,0 +1,65 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    alias(libs.plugins.multiplatform)
+    alias(libs.plugins.compose)
+    alias(libs.plugins.buildConfig)
+}
+
+kotlin {
+    jvm("desktop")
+
+    sourceSets {
+        all {
+            languageSettings {
+                optIn("org.jetbrains.compose.resources.ExperimentalResourceApi")
+            }
+        }
+        val commonMain by getting {
+            dependencies {
+                implementation(compose.runtime)
+                implementation(compose.foundation)
+                implementation(compose.material)
+                @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
+                implementation(compose.components.resources)
+                implementation(libs.voyager.navigator)
+                implementation(libs.napier)
+                implementation(libs.kotlinx.coroutines.core)
+                implementation(libs.composeIcons.featherIcons)
+                implementation(libs.kotlinx.datetime)
+                implementation(libs.koin.core)
+            }
+        }
+
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+
+        val desktopMain by getting {
+            dependencies {
+                implementation(compose.desktop.common)
+                implementation(compose.desktop.currentOs)
+            }
+        }
+
+    }
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "org.soloqueue.app.desktopApp"
+            packageVersion = "1.0.0"
+        }
+    }
+}
+
+buildConfig {
+  // BuildConfig configuration here.
+  // https://github.com/gmazzo/gradle-buildconfig-plugin#usage-in-kts
+}
